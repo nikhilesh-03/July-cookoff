696308951baadc27fb6f402e5ce7542dff997b44
Nikhilesh Vardhan Gupta <78022273+nikhilesh-03@users.noreply.github.com>
2021-07-30 17:51:29 +0530

    Add files via upload

---
+++ bb.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    n=int(input())
+    li=list(map(int, input().split( )))
+    p=max(li)
+    a=0
+    for i in range(len(li)):
+        k=li[i]^p
+        a=k|a
+    print(p)
+    print(a)
